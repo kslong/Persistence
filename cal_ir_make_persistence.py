0ba622be58565ec7291b59d8c9c99f430b4e5eae
Alex Viana <alexcostaviana@gmail.com>
2013-10-26 20:53:58 -0400

    Changed some path dependecies in cal_ir_make_persistence to point to the correct log area and the local git repo on the VM

--- cal_ir_make_persistence.py
+++ cal_ir_make_persistence.py
@@ -38,7 +38,7 @@ def persistence_wrapper_main():
     foo = Chdir('/grp/hst/wfc3a/persistence/workspace')
     logging.info(os.getcwd())
 
-    code_path = '/grp/hst/wfc3a/software_git/automated_scripts/cal_ir_make_persistence'
+    code_path = '/home/viana/ql_code/automated_scripts/cal_ir_make_persistence'
 
     # Run per_list.py
     logging.info('Running: python {}'.format(os.path.join(code_path, 'per_list.py')))
@@ -77,5 +77,5 @@ def persistence_wrapper_main():
 
 
 if __name__ == '__main__':
-    configure_logging('persistence_wrapper')
+    configure_logging('cal_ir_make_persistence')
     persistence_wrapper_main()
