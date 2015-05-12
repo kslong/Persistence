2f18a66cbf0f5ff48254fd979ad922f991424bb5
Knox S. Long <long@stsci.edu>
2015-05-12 08:55:01 -0500

    Change in config to indicate a new version number.  This is to facilliate a quick fix to put the version number in the html outputs

--- config.py
+++ config.py
@@ -23,10 +23,15 @@ Notes:
 									   
 History:
 
-150306 ksl Coding begun so that one could hae access
-to the version number anywhere throught out the 
-persistence routines
+150306 ksl	Coding begun so that one could hae access
+		to the version number anywhere throught out the 
+		persistence routines.  The first version 
+		controlled by this routine is version 3.0
+150513 ksl	Changed version number to 3.0.1 in order to allow
+		a fix to be installed that would record
+		the version number in the .html file
+
 
 '''
 
-version=3.0
+version=3.0.1
